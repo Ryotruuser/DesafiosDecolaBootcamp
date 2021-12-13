b014a1502b1c44e9c84889a02397d1b925bf0786
Ryotruuser <ryonantruuser@gmail.com>
2021-12-13 19:47:25 -0300

    commit init

---
+++ Program.cs
@@ -0,0 +1,95 @@
+﻿using System; 
+
+class DIO 
+{
+  static void Main(string[] args)
+  { 
+    //#6
+    // int su;
+    // int numero = int.Parse(Console.ReadLine());
+    // if(numero % 2 == 0)
+    // {
+    //     su = numero + 2;
+    // }
+    // else
+    // {
+    //     su = numero + 1;
+    // }
+    // System.Console.WriteLine(su);
+
+
+    //#5
+    // string str;
+    // while ((str=Console.ReadLine())!= null) 
+    // {
+    //     int x = int.Parse(str);
+    //     if (x == 0)
+    //     {
+    //         System.Console.WriteLine("vai ter copa!");
+    //     }
+    //     else
+    //     {
+    //         System.Console.WriteLine("vai ter duas!");    
+    //     } 
+    // }   
+
+    //#4
+    // System.Console.WriteLine("Solução com while");
+    // int i = 1;
+	// while (i <= 9) {
+	// 	for (int j = 7; j > 4; j--) {
+	// 		System.Console.WriteLine($"i={i} j={j}");
+	// 	}
+	// 	i+=2;
+	// }
+
+
+    // System.Console.WriteLine("Solução com for");
+    //  for (int a = 1; a <= 9; a++)
+    //  {   
+    //      if(a%2==1)
+    //      {   
+    //         System.Console.WriteLine($"i={a} j={7} ");
+    //          for (int j = 1; j < 3; j++)
+    //          {
+    //              System.Console.WriteLine($"i={a} j={7-j}");
+    //          }
+    //      }
+        
+    // }
+
+    //#3
+    // double c, S= 0;
+    // for (int i = 1; i <= 100; i++)
+    // {
+    //     c = 1; //coloque a sua lógica nos espaços em branco
+    //     S += c / i;
+    // }
+    // var x=Math.Round(S,2);
+    // Console.WriteLine("{0:N2}", ((double)S));
+
+    //#2
+    // int n = int.Parse(Console.ReadLine());
+    // System.Console.WriteLine();
+    // int value = 2;
+    // for (int i = 1; i <= n; i++)
+    // {
+    //     if(i%2==0)
+    //     {
+    //         Console.WriteLine($"{i}^{value} = {(long)Math.Pow(i, value):N0}");
+    //     }
+    // }
+
+    //#1
+    // int N = Convert.ToInt32(Console.ReadLine());
+    // while(N-- > 0)
+    // {
+    //     int val = 7;
+    //     int newval = 5 ; 
+    //     int resposta = newval;
+    //     Console.WriteLine(resposta);
+    // }
+
+  }
+
+}
